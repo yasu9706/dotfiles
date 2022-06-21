b1c7505d4586a740470274de8382209d023ae3cb
yasu <araucaria321@gmail.com>
2022-06-21 12:33:07 +0900

    ファイル等の整理

--- dotfilesLink.sh
+++
@@ -1,3 +0,0 @@
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/colors ~/.vim/colors
-cp gallery-dl.json ~/.config/gallery-dl/config.json
---
+++ lin/dotfilesLink.sh
@@ -0,0 +1,4 @@
+mkdir ~/.vim ~/.config
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/colors ~/.vim/colors
+cp gallery-dl.json ~/.config/gallery-dl/config.json
